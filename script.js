9b955f3f9cd9836e239cb1f9c69b107f970f791a
magidian <manmandoo123@gmail.com>
2022-02-20 23:54:02 +0800

    handle connections

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 // get socket variable using location of 
 // where the socket is hosted
 const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
 // whenever the event is received, call the function
 socket.on('chat-message', data => {
-    console.log(data)
+    // console.log(data)
+    appendMessage(`${data.name}: ${data.message}`)
 }) 
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    appendMessage(`You: ${message}`)
     // send the msg from client to the server
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const io = require('socket.io')(3000, {
     }
 })
 
+const users = {}
+
 // when website is loaded, this function is called and give each user a socket
 io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
     socket.on('send-chat-message', message => {
         // console.log(message)
-        socket.broadcast.emit('chat-message', message)
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
     })
 })
