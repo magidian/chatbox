4416056afe2a89e8d59f08b5a9223aa5a0663437
magidian <manmandoo123@gmail.com>
2022-02-20 23:07:16 +0800

    sending messages to clients

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // get socket variable using location of 
 // where the socket is hosted
 const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
 // whenever the event is received, call the function
 socket.on('chat-message', data => {
     console.log(data)
+}) 
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    // send the msg from client to the server
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
 })
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // create server on port 3000
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: "*",
+    }
+})
 
 // when website is loaded, this function is called and give each user a socket
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
+    socket.on('send-chat-message', message => {
+        // console.log(message)
+        socket.broadcast.emit('chat-message', message)
+    })
 })
